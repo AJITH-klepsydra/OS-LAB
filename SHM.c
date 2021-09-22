66c806032f0634d98c6aed2af7a4e688e3cbf26a
Ajith-Klep <ajithpmuralidharan01@gmail.com>
2021-09-22 15:44:28 +0530

    Update SHM.c

--- SHM.c
+++ SHM.c
@@ -2,27 +2,34 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <stdio.h>
-
+#include<stdlib.h>
 int main(){
 	key_t token = ftok("Token",65);
 	int shmId = shmget(token,1024,0666|IPC_CREAT);
 	char * str = (char *) shmat(shmId,(void *)0,0);
 	int choice;
-	printf("Produce(1) or Consume(0) : ");
+	while(1){
+	printf("\nProduce(1) or Consume(0) or Stop : ");
 	scanf("%d",&choice);
-	if(choice)
+	if(choice ==1)
 		{
-		printf("Data For the shared memory: ");
-		scanf("%s",str);
-		printf("\nDetaching...\n");
-		shmdt(str);
+		int data = rand();
+		printf("Data For the shared memory: %d",data);
+		sprintf(str, "%d", data);
+		
 		}
-	else
+	else if(choice == 0)
 	{
 		printf("\nVALUE IN THE SHM: %s\n",str);
+		
+		shmctl(shmId,IPC_RMID,NULL);
+		}
+		else{
 		printf("\nDetaching ...\nKilling...\n");
 		shmdt(str);
 		shmctl(shmId,IPC_RMID,NULL);
+		break;
+		}
 		}
 	
 
